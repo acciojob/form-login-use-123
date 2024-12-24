fb124af266ad017bbfd75556aeabea2586b1fbcc
use-123 <anmolbejugam@gmail.com>
2024-12-24 17:36:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+    // Get the form elements by their IDs or names
+    const firstName = document.querySelector('input[name="firstName"]').value;
+    const lastName = document.querySelector('input[name="lastName"]').value;
 
+    // Display an alert with the concatenated full name
+    alert(firstName + " " + lastName);
 }
